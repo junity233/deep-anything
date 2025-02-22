80785c5e02ee231bf37de196c60321af15e4e43d
Junity <1737636624@qq.com>
2025-02-22 16:15:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="deepanything",
-    version="0.1.2",
+    version="0.1.5",
     author="Junity",
     author_email="1727636624@qq.com",
     description="DeepAnything is a project that provides DeepSeek R1's deep thinking capabilities for various large language models (LLMs).",
