695396395364d83afb30f84efda5ffca16b78ff7
Junity <1737636624@qq.com>
2025-02-22 17:00:24 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from .deepanything.metadatas import VERSION
+from deepanything.metadatas import VERSION
 
 with open("README.md",encoding='utf-8') as f:
     long_description = f.read()
